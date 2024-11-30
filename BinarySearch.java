3546fda3755492029795bcd5f6ffb322085499b6
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-11-30 22:25:33 +0530

    BinarySearch Starts

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+    //33. Search in Rotated Sorted Array
+    public int search(int[] nums, int x) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == x) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= x && nums[mid] >= x) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= x && nums[high] >= x) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
