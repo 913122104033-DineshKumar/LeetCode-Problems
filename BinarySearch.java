ae2c8a61f561eaea2bbb17d1c182cf4395053fe5
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-01 00:41:15 +0530

    Floor and Ceil Function - Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -23,4 +23,37 @@ public class BinarySearch {
         }
         return -1;
     }
+    public static int[] getFloorAndCeil(int[] a, int n, int x) {
+        int getCeil(int[] a, int n, int x) {
+            int low = 0, high = n - 1;
+            int ans = -1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (a[mid] >= x) {
+                ans = a[mid];
+                high = mid - 1;
+                } else {
+                low = mid + 1;
+                }
+            }
+            return ans;
+        }
+        int getFloor(int[] a, int n, int x) {
+            int low = 0, high = n - 1;
+            int ans = -1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (a[mid] <= x) {
+                    ans = a[mid];
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            return ans;
+        }
+        int floor = getFloor(a, n, x);
+        int ceil = getCeil(a, n, x);
+        return new int[]{ floor, ceil };
+    }
 }
