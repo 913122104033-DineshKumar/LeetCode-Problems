c4592ebb0bfa461b82af3a4889561463d38d6a73
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-03 00:15:52 +0530

    Square Root of X

--- BinarySearch.java
+++ BinarySearch.java
@@ -149,4 +149,22 @@ public class BinarySearch {
         }
         return -1;
     }
+    //69. Sqrt(x)
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        long low = 1, high = x;
+        long ans = 1;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int) ans;
+    }
 }
