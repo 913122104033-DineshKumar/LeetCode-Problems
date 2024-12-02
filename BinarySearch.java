07ff081fd24f9e84d840eda0c761fc733574210a
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-02 17:23:17 +0530

    Minimum Value in Rotated Sorted Array

--- BinarySearch.java
+++ BinarySearch.java
@@ -84,4 +84,20 @@ public class BinarySearch {
         }
         return false;
     }
+    //153. Find Minimum in Rotated Sorted Array
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[low] <= nums[mid]) {
+                ans = Math.min(ans, nums[low]);
+                low = mid + 1;
+            } else {
+                ans = Math.min(ans, nums[mid]);
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
 }
