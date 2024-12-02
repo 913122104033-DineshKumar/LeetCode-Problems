d29911a6bd2b9e7f1b6fc7b140e7653126f65679
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-02 21:31:41 +0530

    Find K Rotations in the Rotated Sorted Array

--- BinarySearch.java
+++ BinarySearch.java
@@ -100,4 +100,27 @@ public class BinarySearch {
         }
         return ans;
     }
+    //Find K Rotations
+    public static int findKRotation(int []nums){
+        int low = 0, high = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int idx = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[low] <= nums[mid]) {
+                if (ans > nums[low]) {
+                    ans = nums[low];
+                    idx = low;
+                }
+                low = mid + 1;
+            } else {
+                if (ans > nums[mid]) {
+                    ans = nums[mid];
+                    idx = mid;
+                }
+                high = mid - 1;
+            }
+        }
+        return idx;
+    }
 }
