9104a3b8382be8e27c0c17d36acf34529e038af7
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-03 08:44:53 +0530

    Find Nth Root of M

--- BinarySearch.java
+++ BinarySearch.java
@@ -192,4 +192,33 @@ public class BinarySearch {
         }
         return -1;
     }
+    //Find Nth Root of M
+    public static int NthRoot(int n, int m) {
+        int getExponential(int mid, int n, int m) {
+            long ans = 1;
+            for (int i = 1; i <= n; i++) {
+                ans *= mid;
+                if (ans > m) {
+                    return 2;
+                } 
+            }
+            if (ans < m) {
+                return 1;
+            }
+            return 0;
+        }
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int ans = getExponential(mid, n, m);
+            if (ans == 0) {
+                return mid;
+            } else if (ans == 2) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
