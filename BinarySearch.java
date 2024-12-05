b877157e442600040a66f73bb53a35f40601239a
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-06 00:43:17 +0530

    Kth Missing Positive Number

--- BinarySearch.java
+++ BinarySearch.java
@@ -312,4 +312,18 @@ public class BinarySearch {
         }
         return low;
     }
+    //Kth Missing Positive Number
+    public static int missingK(int[] vec, int n, int k) {
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int missing = vec[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low + k;
+    }
 }
