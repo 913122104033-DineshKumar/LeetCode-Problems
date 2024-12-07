27ef62a3811b3cb16338a7e70d06d9d4d4a14945
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-07 18:13:03 +0530

    Median of two sorted arrays

--- BinarySearch.java
+++ BinarySearch.java
@@ -448,4 +448,44 @@ public class BinarySearch {
         }
         return res;
     }
+    // Median of two sorted arrays
+    public static double median(int[] a, int[] b) {
+    	int n1 = a.length;
+        int n2 = b.length;
+        int n = n1 + n2;
+        if (n2 < n1) {
+            return median(b, a);
+        }
+        int low = 0, high = n1;
+        int left = (n + 1) / 2;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1= Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if (mid1 < n1) {
+                r1 = a[mid1];
+            }
+            if (mid2 < n2) {
+                r2 = b[mid2];
+            }
+            if (mid1 > 0) {
+                l1 = a[mid1 - 1];
+            }
+            if (mid2 > 0) {
+                l2 = b[mid2 - 1];
+            }
+            if (l1 > r2) {
+                high = mid1 - 1;
+            } else if (l2 > r1) {
+                low = mid1 + 1;
+            } else {
+                if (n % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+                return Math.max(l1, l2);
+            }
+        }
+        return 0;
+    }
 }
