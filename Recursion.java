c7d0fd28dae1c95d4c54a13c28a149f963cca24a
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-12-15 12:18:18 +0530

    Suduko Problem

--- Recursion.java
+++ Recursion.java
@@ -180,4 +180,43 @@ public class Recursion {
         permutate(0, nums, ans);
         return ans;
     }
+    //Sudoku Problem
+    public void solveSudoku(char[][] board) {
+        boolean solve(char[][] board, int n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (board[i][j] == '.') {
+                        for (char val = '1'; val <= '9'; val++) {
+                            if (isValid(board, i, j, val)) {
+                                board[i][j] = val;
+                                if (solve(board, n)) {
+                                    return true;
+                                }
+                                else {
+                                    board[i][j] = '.';
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }   
+            return true;
+        }
+        boolean isValid(char[][] board, int row, int col, char c) {
+            for (int i = 0; i < 9; i++) {
+                if (board[row][i] == c) {
+                    return false;
+                }   
+                if (board[i][col] == c) {
+                    return false;
+                }
+                if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        solve(board, 9);
+    }
 }
