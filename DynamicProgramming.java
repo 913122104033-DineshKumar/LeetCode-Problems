b4fde64960c1249f95be59aa99bac5229a3fbbff
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2025-04-17 10:48:10 +0530

    Unique Paths II

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -187,4 +187,45 @@ public class DynamicProgramming {
         return dp[n - 1];
     }
 
+    // Memoization
+    private int uniquePathsWithObstaclesMemoization (int row, int col, int[][] dp, int[][] grid) {
+        if (row == 0 && col == 0 && grid[row][col] != 1) {
+            return 1;
+        }
+        if (row < 0 || col < 0 || grid[row][col] == 1) {
+            return 0;
+        }
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        int up = uniquePathsWithObstaclesMemoization(row - 1, col, dp, grid);
+        int left = uniquePathsWithObstaclesMemoization(row, col - 1, dp, grid);
+        return dp[row][col] = up + left;
+    }
+
+    // Tabulation
+    private int uniquePathsWithObstaclesTabulation (int m, int n, int[][] grid) {
+        int[][] dp = new int[m][n];
+        if (grid[0][0] != 1) {
+            dp[0][0] = 1;
+        }
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row == 0 && col == 0 && grid[row][col] != 1) {
+                    dp[row][col] = 1;
+                } else {
+                    int up = 0, left = 0;
+                    if (row > 0 && grid[row][col] != 1) {
+                        up = dp[row - 1][col];
+                    }
+                    if (col > 0 && grid[row][col] != 1) {
+                        left = dp[row][col - 1];
+                    }
+                    dp[row][col] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
 }
