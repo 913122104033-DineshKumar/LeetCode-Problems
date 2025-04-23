170cdab251a60b63d208736231c9031320afc3a3
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2025-04-23 08:17:11 +0530

    Min Operations to make Strings Equal

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -859,4 +859,22 @@ public class DynamicProgramming {
         return n - dp[n][n];
     }
 
+    public int minDistanceToMakeStringsEqual(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[] dp = new int[m + 1];
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            int[] cur = new int[m + 1];
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1)) {
+                    cur[ind2] = 1 + dp[ind2 - 1];
+                } else {
+                    cur[ind2] = Math.max(dp[ind2], cur[ind2 - 1]);
+                }
+            }
+            dp = cur;
+        }
+        return n + m - (2 * dp[m]);
+    }
+
 }
